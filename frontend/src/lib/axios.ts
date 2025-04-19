00555794b5a1f5f1c15ab87ebe2f32fc1f7efb28
satvik8373 <satvikpatel8373@gmail.com>
2025-04-19 20:32:38 +0530

    c

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,5 +1,9 @@
+/// <reference types="vite/client" />
+
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-	baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+	baseURL: import.meta.env.MODE === "development" 
+		? "http://localhost:5000/api" 
+		: "https://spotify-6ii0e5rf2-satvik8373s-projects.vercel.app/api",
 });
