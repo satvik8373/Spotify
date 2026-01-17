dc0a07fce6253f89e15dd25dc4a40b0fac3f4ca6
Satvik Patel <satvikpatel8373@gmail.com>
2026-01-18 01:06:50 +0530

    fg

--- frontend/src/layout/components/PlaybackControls.tsx
+++ frontend/src/layout/components/PlaybackControls.tsx
@@ -379,7 +379,6 @@ export const PlaybackControls = () => {
 					</div>
 				</div>
 			</footer>
-<<<<<<< HEAD
 
 			{/* Mobile Player */}
 			<div
@@ -476,8 +475,6 @@ export const PlaybackControls = () => {
 					/>
 				</div>
 			</div>
-=======
->>>>>>> 563e72b99457c6add84a7c5c77100ecf29a18dc2
 		</>
 	);
 };
